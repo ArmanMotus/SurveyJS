7bbc66867790ef5344282d64926aaa8f16fed7c8
ArmanMotus <arman@datamotus.com>
2024-06-25 13:46:04 +0400

    Update CORS configuration

--- server/server.js
+++ server/server.js
@@ -8,9 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://motustest.netlify.app', // Replace with your Netlify domain
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type']
+}));
 
-// Serve static files from the public directory
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.post('/submit-survey', (req, res) => {
